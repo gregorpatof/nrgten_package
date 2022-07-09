4f49cd60f2abd5720487196f409fb94d1621ea83
gregorpatof <shulgin@pop-os.localdomain>
2022-07-09 14:03:23 -0400

    round 2 parameter optimization

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(os.path.join(HERE, "README.md")) as f:
 
 setuptools.setup(
     name="nrgten",
-    version="1.1.13",
+    version="1.2.0",
     author="Olivier Mailhot",
     description="Najmanovich Research Group Toolkit for Elastic Networks",
     long_description=README,
